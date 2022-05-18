1ddb87dccf2ed91c7c3978eb474d62eb6b23ee9d
Your Name <xd9722@gmail.com>
2022-05-18 22:56:09 +0800

    first commit

---
+++ bing-url.md
@@ -0,0 +1,2 @@
+20220517 | [护城河边的格吕克斯堡城堡，德国 (© Christian Back/eStock Photo)](https://cn.bing.com/th?id=OHR.SchlossGluecksburg_ZH-CN4079837227_UHD.jpg)
+
---
+++ main.py
@@ -0,0 +1,26 @@
+import tool
+
+
+with open('README.md', 'w+')as fw:
+    fw.write(tool.get_date()[0]+'\n')
+    fw.write('|      |      |      |\n')
+    fw.write('| :----: | :----: | :----: |\n')
+    i = 0
+    for dd in tool.get_list():
+        i = i+1
+        fw.write('|')
+        fw.write(dd)           
+        if (i % 3 == 0):
+            fw.write("|\n")
+    if(len(tool.get_list()) % 3 != 0):
+        fw.write('|')
+
+
+with open('bing-url.md', 'r+')as fi:
+    content = fi.read()        
+    fi.seek(0, 0)
+    fi.write(tool.get_date()[1]+'\n\n'+content)
+    # fi.write(tool.get_date()[1])
+
+
+# print()
